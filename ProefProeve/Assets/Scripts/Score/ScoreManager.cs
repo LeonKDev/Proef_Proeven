e6361347e74ffc5b6e330657105acef3fee2135b
stokbrootje <Leonkruijer@outlook.com>
2025-03-04 15:10:09 +0100

    Revert "Added score counter and imported TMpro"
    This reverts commit a6bea69c3acd3c914868ef132123a1142956d584.

--- ProefProeve/Assets/Scripts/Score/ScoreManager.cs
+++ ProefProeve/Assets/Scripts/Score/ScoreManager.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -9,10 +8,6 @@ public class ScoreManager : MonoBehaviour
     private int _score = 0;
     private int _highScore = 0;
     
-    //References for UI
-    [Header("UI References")]
-    [SerializeField] private TextMeshProUGUI ScoreText;
-    [SerializeField] private TextMeshProUGUI HighscoreText;
     private void Awake()
     {
         Instance = this;
@@ -21,22 +16,16 @@ public class ScoreManager : MonoBehaviour
     private void Start()
     {
         _highScore = PlayerPrefs.GetInt("highscore", 0);
-        
-        ScoreText.text = _score.ToString("00000");
-        HighscoreText.text = _highScore.ToString("00000");
     }
 
     public void AddPoints(int amount)
     {
         _score += amount;
-        ScoreText.text = _score.ToString("00000");
-        
-        if (_highScore > _score)
-            return;
-        
-        // saves the highest score outside the game loop
-        PlayerPrefs.SetInt("highscore", _score);
-        
+        if (_highScore < _score)
+        {
+            // saves the highest score outside the game loop
+            PlayerPrefs.SetInt("highscore", _score);
+        }
     }
 }
 
