b95651a4774bae2c2c7d59209ab1e74ee663fd9c
LeonKDev <Leonkruijer@outlook.com>
2025-03-10 12:18:12 +0100

    comments

--- ProefProeve/Assets/Scripts/Score/ScoreManager.cs
+++ ProefProeve/Assets/Scripts/Score/ScoreManager.cs
@@ -5,7 +5,6 @@ public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance;
     
-    // Score storage
     private int _score = 0;
     private int _highScore = 0;
     
@@ -13,6 +12,14 @@ public class ScoreManager : MonoBehaviour
     [Header("UI References")]
     [SerializeField] private TextMeshProUGUI ScoreText;
     [SerializeField] private TextMeshProUGUI HighscoreText;
+    
+    //Property for accessing score safely 
+    public int Score
+    {
+        get => _score;
+        private set => _score = value;
+    }
+    
     private void Awake()
     {
         Instance = this;
@@ -22,19 +29,22 @@ public class ScoreManager : MonoBehaviour
     {
         _highScore = PlayerPrefs.GetInt("highscore", 0);
         
-        ScoreText.text = _score.ToString("00000");
+        ScoreText.text = Score.ToString("00000");
         HighscoreText.text = _highScore.ToString("00000");
     }
-
+    
+    /// <summary>
+    /// Class <c>AddPoints</c> Adds amount to the current score.
+    /// </summary>
     public void AddPoints(int amount)
     {
-        _score += amount;
-        ScoreText.text = _score.ToString("00000");
+        Score += amount;
+        ScoreText.text = Score.ToString("00000");
         
-        if (_highScore > _score)
+        if (_highScore > Score)
             return;
         
         // saves the highest score outside the game loop
-        PlayerPrefs.SetInt("highscore", _score);
+        PlayerPrefs.SetInt("highscore", Score);
     }
 }
